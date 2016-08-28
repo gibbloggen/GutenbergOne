bda435e5d3fb04af7979bfc6b5c36260086c5b67
gibbloggen <gibbloggen@gmail.com>
2016-08-27 21:36:40 -0400

    Add project files.

---
+++ GutenbergOne/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.Graphics.Printing;
+
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GutenbergOne
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private PrintHelper printHelper;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private async void OnPrintButtonClick(object sender, RoutedEventArgs e)
+        {
+            await printHelper.ShowPrintUIAsync();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (PrintManager.IsSupported())
+            {
+                // Tell the user how to print
+                //jl  MainPage.Current.NotifyUser("Print contract registered with customization, use the Print button to print.", NotifyType.StatusMessage);
+            }
+            else
+            {
+                // Remove the print button
+                InvokePrintingButton.Visibility = Visibility.Collapsed;
+
+                // Inform user that Printing is not supported
+                //jl  MainPage.Current.NotifyUser("Printing is not supported.", NotifyType.ErrorMessage);
+
+                // Printing-related event handlers will never be called if printing
+                // is not supported, but it's okay to register for them anyway.
+            }
+
+            // Initalize common helper class and register for printing
+            printHelper = new PrintHelper(this);
+            printHelper.RegisterForPrinting();
+
+            // Initialize print content for this scenario
+
+          
+            printHelper.PreparePrintContent(new Radio());
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            if (printHelper != null)
+            {
+                printHelper.UnregisterForPrinting();
+            }
+        }
+    }
+}
